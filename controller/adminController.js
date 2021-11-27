f04e6788f133029517e4e15551d772ad1d040231
Toshpolat <okmkhk@gmail.com>
2021-11-27 16:46:45 +0500

    admin route 1

--- controller/adminController.js
+++ controller/adminController.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
 const Category = require('../models/Category')
-const Work = require('../models/Work')
+const Worker = require('../models/Worker')
 const toDelete = require('../middleware/toDelete')
 const moment = require('moment');
--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,40 @@
 const { Router } = require('express')
 const router = Router()
-const mongoose = require('mongoose')
 const fileMiddleware = require("../middleware/fileUpload")
 const adminController = require("../controller/adminController")
 
+router.get('/', adminController.mainAdmin);
 
+/* +++++++++++++++++++++++++++++++++++++++ Category ++++++++++++++++++++++++++++++++++++++++++++++ */
+
+router.get('/category', adminController.GetReadCategory);
+
+router.get('/category/add', adminController.GetAddCategory);
+
+router.get('/category/:id', adminController.GetIDCategory);
+
+router.get('/category/edit/:id', adminController.GetEditCategory);
+
+router.post('/category/add', fileMiddleware.single("img"), adminController.PostAddCategory);
+
+router.post("/category/edit/:id", fileMiddleware.single("img"), adminController.PostEditCategory);
+
+router.get("/category/delete/:id", adminController.GetDeleteCategory);
+
+/* +++++++++++++++++++++++++++++++++++++++ Worker ++++++++++++++++++++++++++++++++++++++++++++++ */
+
+router.get("/worker", adminController.GetReadWorker);
+
+router.get('/worker/add', adminController.GetAddWorker);
+
+router.get("/worker/:id", adminController.GetIDWorker);
+
+router.get("/worker/edit/:id", adminController.GetEditWorker);
+
+router.post('/worker/add', fileMiddleware.single("img"), adminController.PostAddWorker);
+
+router.post("/worker/edit/:id", fileMiddleware.single("img"), adminController.PostEditWorker);
+
+router.get("/worker/delete/:id", adminController.GetDeleteWorker);
 
 module.exports = router;
