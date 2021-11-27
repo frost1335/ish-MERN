9d649416bf40c122ad07c1abd9a0d30525fa1c2c
raximvodil <raximvodilrozbek@gmail.com>
2021-11-27 16:09:48 +0500

    mongodb added

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const app = express();
 
 app.use(express.json());
 
+require("./helper/db")();
+
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => console.log(`👏 Server running on port ${PORT}...`));
