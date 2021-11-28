54a691c31bdbfbed71fb3b0351417269b01a0927
Toshpolat1225 <turaqulovtoshpulat@mail.ru>
2021-11-28 14:14:45 +0500

    new admin route

--- routes/admin.js
+++ routes/admin.js
@@ -3,38 +3,39 @@ const router = Router()
 const fileMiddleware = require("../middleware/fileUpload")
 const adminController = require("../controller/adminController")
 
-router.get('/', adminController.mainAdmin);
-
+router
+    .route("/")
+    .get(adminController.mainAdmin);
+    
 /* +++++++++++++++++++++++++++++++++++++++ Category ++++++++++++++++++++++++++++++++++++++++++++++ */
 
-router.get('/category', adminController.GetReadCategory);
+router
+    .route("/category")
+    .get(adminController.GetReadCategory);
 
-router.get('/category/add', adminController.GetAddCategory);
-
-router.get('/category/:id', adminController.GetIDCategory);
-
-router.get('/category/edit/:id', adminController.GetEditCategory);
-
-router.post('/category/add', fileMiddleware.single("img"), adminController.PostAddCategory);
-
-router.post("/category/edit/:id", fileMiddleware.single("img"), adminController.PostEditCategory);
-
-router.get("/category/delete/:id", adminController.GetDeleteCategory);
+router
+    .route("/category/add")
+    .get(adminController.GetAddCategory)
+    .post(fileMiddleware.single("img"), adminController.PostAddCategory);
+router
+    .route("/category/:id")
+    .get(adminController.GetIDCategory)
+    .put(fileMiddleware.single("img"), adminController.PostEditCategory)
+    .delete(adminController.GetDeleteCategory);
 
 /* +++++++++++++++++++++++++++++++++++++++ Worker ++++++++++++++++++++++++++++++++++++++++++++++ */
 
-router.get("/worker", adminController.GetReadWorker);
-
-router.get('/worker/add', adminController.GetAddWorker);
-
-router.get("/worker/:id", adminController.GetIDWorker);
-
-router.get("/worker/edit/:id", adminController.GetEditWorker);
-
-router.post('/worker/add', fileMiddleware.single("img"), adminController.PostAddWorker);
-
-router.post("/worker/edit/:id", fileMiddleware.single("img"), adminController.PostEditWorker);
-
-router.get("/worker/delete/:id", adminController.GetDeleteWorker);
+router
+    .route("/worker")
+    .get(adminController.GetReadWorker);
+router
+    .route("/worker/add")
+    .get(adminController.GetAddWorker)
+    .post(fileMiddleware.single("img"), adminController.PostAddWorker);
+router
+    .route("/worker/:id")
+    .get(adminController.GetIDWorker)
+    .put(fileMiddleware.single("img"), adminController.PostEditWorker)
+    .delete(adminController.GetDeleteWorker);
 
 module.exports = router;
