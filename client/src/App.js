c9707bd5c6df2f79ebd83f0350a028b750b26533
raximvodil <raximvodilrozbek@gmail.com>
2021-11-28 18:21:18 +0500

    routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,9 @@
+import useRoutes from "./routes";
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const router = useRoutes();
+
+  return <div className="App">{router}</div>;
 }
 
 export default App;
--- client/src/routes.js
+++ client/src/routes.js
@@ -1,12 +1,15 @@
-import React from 'react'
-import {BrowserRouter} from 're'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-const routes = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+const useRoutes = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>Hello</h1>} />
+        <Route path="/add" element={<h1>Add</h1>} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default routes
+export default useRoutes;
