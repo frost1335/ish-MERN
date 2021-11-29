58aadee61cc4420f6ca2a05f9e3f7dd8bdc2d332
raximvodil <raximvodilrozbek@gmail.com>
2021-11-29 18:54:40 +0500

    front edited

---
+++ client/src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container, Grid } from "@material-ui/core";
+
+const Home = () => {
+  return (
+    <Container>
+      <Grid item={true} xs={2}>
+        <h1>asd</h1>
+      </Grid>
+    </Container>
+  );
+};
+
+export default Home;
---
+++ client/src/pages/Workers.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Workers = () => {
+  return (
+    <div>
+      <h1>Workers</h1>
+    </div>
+  );
+};
+
+export default Workers;
